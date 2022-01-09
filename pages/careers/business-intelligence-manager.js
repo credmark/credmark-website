2db029fe1151a30549c305ae8ce8dde82dc761dc
Elektra Murphy <elektra.murphy@icloud.com>
2022-01-09 17:32:48 +0100

    Revert "Roboto css updated"
    This reverts commit 34239af8a76b652d7e4eb1f69615002c53e48579.

--- pages/careers/business-intelligence-manager.js
+++ pages/careers/business-intelligence-manager.js
@@ -28,10 +28,6 @@ export default function BusinessIntelligenceManagerPage() {
           `,
             }}
           />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet" />
       </Head>
 
       <div className="nav">
