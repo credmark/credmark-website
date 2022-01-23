5f44bcfdcc715fba2b38910ed4f8e0c488e9af2b
Elektra Murphy <elektra.murphy@icloud.com>
2022-01-23 18:44:17 +0000

    Hotjar & GA re-installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import '../styles/globals.css'
+import * as gtag from '../lib/gtag'
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  useEffect(() => {
+    hotjar.initialize(2794251, 6)
+  }, [])
+
   return <Component {...pageProps} />
 }
 
