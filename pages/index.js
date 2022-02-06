31d61a4ac9e10e41e8b0b15b363ed119248ea90e
Elektra Murphy <elektra.murphy@icloud.com>
2022-02-06 11:16:53 +0000

    Analytics code added

--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,23 @@ export default function Home() {
         <meta property="og:image" content="https://credmark.com/assets/credmark-og-image.png" />
         <link rel="icon" href="/favicon.ico" />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=UA-201404361-1`}
+        />
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=UA-201404361-1'
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-201404361-1');`
+          }}
+        ></script>
       </Head>
 
       <div className="nav">
