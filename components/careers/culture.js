189c02b5fed700340702699ea2761e31fb9e2bc3
Elektra Murphy <elektra.murphy@icloud.com>
2022-05-02 14:54:26 +0200

    Error: Missing "key" prop for element in iterator error fix

--- components/careers/culture.js
+++ components/careers/culture.js
@@ -22,16 +22,16 @@ const people = [
   export default function Culture() {
     return (
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
-        {people.map((mission) => (
+        {people.map((people) => (
           <div
             className="relative rounded-lg border border-gray-300 px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
           >
             <div className="flex-shrink-0">
-              <img src={mission.imageUrl} alt={mission.alt} />
+              <img src={people.imageUrl} alt={people.alt} />
             </div>
             <div className="flex-1 min-w-0">
                 <span className="absolute inset-0" aria-hidden="true" />
-                <p className="text-2xl font-light text-white ml-2">{mission.title}</p>
+                <p className="text-2xl font-light text-white ml-2">{people.title}</p>
             </div>
           </div>
         ))}
