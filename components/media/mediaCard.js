d570df9a6cd57c8c707e93f6d4889003a53ec021
Elektra Murphy <elektra.murphy@icloud.com>
2022-05-15 12:00:55 +0100

    git issue #74

--- components/media/mediaCard.js
+++ components/media/mediaCard.js
@@ -11,7 +11,7 @@ const MediaCard = (props) => {
                                 className='object-cover	flex justify-center cursor-pointer rounded-t-md'
                             />
                         </a>
-                        <div className="bg-white shadow-md rounded-b-md">
+                        <div className="bg-white shadow-md rounded-b-md h-72">
                         <p className="text-xs text-black font-bold px-5 pt-6">{props.date}</p>
                         <a href={props.link} target="_blank" rel="noreferrer"><p className="px-5 cursor-pointer text-black text-3xl font-medium pb-2">{props.description}</p></a>
                         <a href={props.link} target="_blank" rel="noreferrer"><p className="px-5 cursor-pointer text-black text-sm flex pb-9">Read more<img className="pl-1" alt="arrow right" src="../assets/arrow.svg" /></p></a>
