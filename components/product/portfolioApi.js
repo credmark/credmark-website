6f679d6d0d3686ed05e7df3a9bc05ecba10b5c17
Elektra Murphy <elektra.murphy@icloud.com>
2022-08-21 09:32:39 -0400

    Portfolio API product page done

--- components/product/portfolioApi.js
+++ components/product/portfolioApi.js
@@ -13,61 +13,62 @@ export default function PortfolioApi() {
 
     return (
         <>
-     <div>
+            <div>
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="space-y-4 sm:space-y-0 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 py-16">
                         <div className="bg-white p-4 lg:p-24 border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200 shadow-lg">
                             <div>
                                 <h2 className="openSansMedium leading-6 text-credmarkpurple text-2xl lg:text-5xl pb-3 pt-10 lg:pt-0">Portfolio API</h2>
-                                <p className="texl-lg font-bold">The Token API is what you need to access everything token related</p>
-                            </div>
-                            <div className="grid lg:grid-cols-4 lg:space-x-4 pb-20">
-                                <div>
-                                    <p className="text-2xl text-lightpurple font-bold pt-9">Prices</p>
-                                    <p className="text-base">Fetch the actual DeFi token
-                                        prices derived from aggregate
-                                        DEX activity or acess our
-                                        external oracle for tokens
-                                        traded on centralized exchanges.</p>
-                                </div>
-
-                                <div>
-                                    <p className="text-2xl text-lightpurple font-bold pt-9">Liquidity</p>
-                                    <p className="text-base">
-                                        Discover where tokens are traded, the best prices, or analyze the liquidity patterns. Our liquidity function will show available liquidity for any token on DEXs at any given time.
-                                    </p>
-                                </div>
-
-                                <div>
-                                    <p className="text-2xl text-lightpurple font-bold pt-9">Concentration</p>
-                                    <p className="text-base">
-                                        Glean insights on token concentration across no. of wallets, the addresses, and know the exact distribution of those holdings in liquidity pools.
-                                    </p>
-                                </div>
-
-                                <div>
-                                    <p className="text-2xl text-lightpurple font-bold pt-9">Metadata</p>
-                                    <p className="text-base">
-                                        Simple access to contract address, number of decimals,
-                                        or even current supply.
-                                    </p>
-                                </div>
+                                <p className="text-base font-bold">You’re dealing with crypto portfolios? You need to show your users what they own, how much it’s worth today, how much it was worth, and how risky the portfolio is? You’ve come to the right place. We’ll give you fast, easy access to the portfolio data your users want.</p>
+                                <p className="text-base font-bold">But not today.</p>
+                                <p className="text-base font-bold">This API is still under development. If this is something you’re going to need, get in touch so that we can make sure we include the data you want. Also, you might want to be part of our private beta.</p>
                             </div>
                             <div>
-                                <h4 className="uppercase text-credmarkpurple text-3xl pb-3">Not sure what to build?</h4>
-                                <p>There are dozens of use cases but we can only list so many.
-                                    <br />
-                                    Check out the examples below</p>
-                                <div className="pt-3 flex">
-                                    <p className="pr-3.5 text-base">
-                                        <span><img className="float-left mr-2" src="/assets/checkedCircle.svg" alt="tick" width={26} />Wallet or dApps</span></p>
-                                    <p className="pr-3.5 text-base">
-                                        <span><img className="float-left mr-2" src="/assets/checkedCircle.svg" alt="tick" width={26} />Trading Platform</span></p>
-                                    <p className="pr-3.5 text-base">
-                                        <span><img className="float-left mr-2" src="/assets/checkedCircle.svg" alt="tick" width={26} />DeFi dashboard</span></p>                            </div>
+                                <p className="uppercase text-credmarkpurple text-3xl pt-10 pb-3 openSansMedium">What you get</p>
+                                <p className="text-base">
+                                    We’re still working on this. Our internal wish list is very long. When we first publish, you’ll be able to:
+                                </p>
+                                <ul>
+                                    <li className="text-base">
+                                        price a portfolio,
+                                    </li>
+                                    <li className="text-base">
+                                        retrieve portfolio activity (cashflow, transactions), and
+                                    </li>
+                                    <li className="text-base">
+                                        retrieve basic risk metrics (VaR and Sharpe ratio ).
+                                    </li>
+                                </ul>
+                                <p className="text-base">
+                                    You could, of course, price the portfolio by making a lot of calls to the Token API, but that could be very costly. If you need the prices of many tokens, or you need prices across a long series of blocks, you’ll definitely want to use this API. In fact, you won’t even have to send us a list of tokens. Send us a list of wallet addresses and we’ll figure out what’s there.
+                                </p>
+                                <p className="text-base">
+                                    Tokens flow in and out of wallets. You could use a Web3 API to get all that information, but it’s complicated. The Portfolio API makes it easy.
+                                </p>
+                                <p className="text-base">
+                                    Traditional finance is very good at managing risk. DeFi isn’t. That’s why we built Credmark. Our DeFi API gives you direct access to all of the financial models built on our platform. Some of those compute risk associated with portfolios. Instead of forcing you to become a DeFi financial modeling expert, we’re going to make the best models available to you through this API.
+                                </p>
                             </div>
+                            <div>
+                                <p className="uppercase text-credmarkpurple text-3xl pt-10 pb-3 openSansMedium">Who needs it?</p>
+                                <p className="text-base">
+                                    Anyone who needs to display users’ portfolio information. Sometimes it’s a lot, like transaction details, and sometimes it’s a little, like the portfolio’s value in USD.
+                                </p>
+                                <p className="text-base">Some common use cases include:</p>
+                                <div>
+                                    <p className="py-5 pr-3.5 text-base">
+                                        <span><img className="float-left mr-4" src="/assets/checkedCircle.svg" alt="tick" width={26} />a lender needing to understand a borrower’s financial situation,</span></p>
+                                    <p className="py-5 pr-3.5 text-base">
+                                        <span><img className="float-left mr-4" src="/assets/checkedCircle.svg" alt="tick" width={26} />a treasury manager needing to track positions over time,</span></p>
+                                    <p className="py-5 pr-3.5 text-base">
+                                        <span><img className="float-left mr-4" src="/assets/checkedCircle.svg" alt="tick" width={26} />a wallet needing to show current and past state, and</span></p>
+                                    <p className="py-5 pr-3.5 text-base">
+                                        <span><img className="float-left mr-4" src="/assets/checkedCircle.svg" alt="tick" width={26} />a portfolio aggregator needing to roll up data from multiple wallets.</span></p>
+                                </div>
+                            </div>
+                            
                             <div className="rounded-md applicationsBg mt-16 py-5 lg:py-20 p-5">
-                                <p className="uppercase text-white text-center font-bold text-3xl pb-2 lg:pb-8">Interact with the Token API</p>
+                                <p className="openSansMedium uppercase text-white text-center font-bold text-3xl pb-2 lg:pb-8">Interact with the Portfolio API</p>
                                 <p className="text-center pb-8">We have a 30 day trial if you’re unsure or start building immediately!</p>
                                 <div className="flex justify-center">
                                     <button
@@ -83,7 +84,7 @@ export default function PortfolioApi() {
                             <div className="pt-5 lg:pt-16">
                                 <h4 className="uppercase text-credmarkpurple text-3xl pb-3 text-center">Build with the community</h4>
                                 <p className="text-center max-w-2xl mx-auto">
-                                We’re a global community of modelers. Join us and engage with other members building models using our API.
+                                    We’re a global community of modelers. Join us and engage with other members building models using our API.
                                 </p>
                                 <div className="flex justify-center mt-8 mb-8 lg:mb-0">
                                     <button
