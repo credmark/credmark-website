da8635ba6ae444fde41dba65640cf16151a987f5
Elektra Murphy <elektramurphy@Elektras-MacBook-Air.local>
2022-09-21 20:59:13 +0100

    Product section tabs re-ordered

--- components/product/applications.js
+++ components/product/applications.js
@@ -17,8 +17,8 @@ function classNames(...classes) {
 
 const Tab = ({ href, isSelected, title }) => (
     <Link href={href}>
-        <div className={isSelected ? "cursor-pointer py-4 px-6 border-b-2 border-green min-h-full" : "cursor-pointer py-4 px-6 border-b-2 border-white min-h-full"}>
-            <a className={isSelected ? "cursor-pointer openSansMedium text-xl font-bold text-green text-center" : "cursor-pointer openSansMedium text-xl font-bold text-white text-center"}>
+        <div className={isSelected ? "text-center  cursor-pointer py-4 px-6 border-b-2 border-green min-h-full" : "text-center cursor-pointer py-4 px-6 border-b-2 border-white min-h-full"}>
+            <a className={isSelected ? "text-center cursor-pointer openSansMedium text-xl font-bold text-green text-center" : "text-center  cursor-pointer openSansMedium text-xl font-bold text-white text-center"}>
                 {title}
             </a>
         </div>
@@ -39,9 +39,9 @@ const Applications = () => {
                 <h1 className="tracking-tight font-bold gradientFont text-center pb-5 lg:pb-11">Product descriptions</h1>
                 <div className="grid grid-cols-2 lg:grid-cols-4 max-w-6xl block mx-auto">
                     <Tab href="/product/?tokenApi=true#learnMore" title="Token API" isSelected={isTabOneSelected} />
-                    <Tab href="/product/?rawData=true#learnMore" title="Raw Data " isSelected={isTabTwoSelected} />
-                    <Tab href="/product/?defiApi=true#learnMore" title="DeFi API" isSelected={isTabThreeSelected} />
                     <Tab href="/product/?portfolioApi=true#learnMore" title="Portfolio API" isSelected={isTabFourSelected} />
+                    <Tab href="/product/?defiApi=true#learnMore" title="DeFi API" isSelected={isTabThreeSelected} />
+                    <Tab href="/product/?rawData=true#learnMore" title="Raw Data " isSelected={isTabTwoSelected} />
                 </div>
 
                 <div className="outlet">
