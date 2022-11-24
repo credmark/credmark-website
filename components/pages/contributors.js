5cddd1aabc615215d3df0ca4b0c3f15bb4a69ef2
Elektra Murphy <elektra.murphy@icloud.com>
2022-11-24 21:44:27 +0100

    git issue #158

--- components/pages/contributors.js
+++ components/pages/contributors.js
@@ -88,6 +88,44 @@ export default function Team() {
                                 twitter="https://twitter.com/wc_su95"
                                 linkedin="https://www.linkedin.com/in/wen-chiao-su/"
                             />
+                                <div className="box col-xs">
+                                <div id="team" className="md:py-10">
+                                    <div>
+                                        <div className="rounded-full img-hover">
+                                            <img className="text-center bio-photo" src="../assets/arunachalam.png" alt="Arunachalam Pandi" />
+                                            <div className="overlay relative">
+                                            <div className="socialButtons flex justify-center">
+                                        <a href="https://twitter.com/Arunpandi_kr" target="_blank" rel="noreferrer">
+                                            <svg className="cursor-pointer" width={25} fill="#ffff" viewBox="0 0 24 24">
+                                                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                                            </svg>
+                                            </a>
+                                        </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <p className="text-black font-bold pt-2.5 pb-0 text-center text-base">Arunachalam Pandi</p>
+                                            <p className="text-black pb-5 pt-0 text-center text-base">Devops</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="box col-xs">
+                                <div id="team" className="md:py-10">
+                                    <div>
+                                        <div className="rounded-full img-hover">
+                                            <img className="text-center bio-photo" src="../assets/kavita.png" alt="Kavita Joshi" />
+                                            <div className="overlay relative">
+
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <p className="text-black font-bold pt-2.5 pb-0 text-center text-base">Kavita Joshi</p>
+                                            <p className="text-black pb-5 pt-0 text-center text-base">Data Engineer</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="advisorBg">
