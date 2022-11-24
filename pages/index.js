2253b56376e149d21eee7741b75a67598626e6ea
Elektra Murphy <elektra.murphy@icloud.com>
2022-11-24 21:46:07 +0100

    Cursor pointer css added to sticky nav CTA

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home({ data }) {
 
       <div className="py-2 stickyCta text-center relative">
         <div>
-          <Link href="/product?tokenApi=true#learnMore" rel="noreferrer"><p className="text-white text-base flex justify-center flex-wrap"><span className="float-left pr-3">
+          <Link href="/product?tokenApi=true#learnMore" rel="noreferrer"><p className="text-white text-base flex justify-center flex-wrap cursor-pointer"><span className="float-left pr-3">
             The only source of DeFi prices: our Token API. Simple. Powerful. Try it.</span><img className="hover:animate-pulse" src="../assets/arrow-white.svg" alt="Right arrow" /></p></Link>        
         </div>
       </div>
