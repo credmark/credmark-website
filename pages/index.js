5b112075bef41290f8bb961acf6897d22b75f146
Elektra Murphy <elektra.murphy@icloud.com>
2023-03-01 20:25:37 +0000

    Logos updated

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import React from "react";
 import Nav from '../components/layout/nav'
 import TryApi from '../components/homepage/tryApi'
 import BuildModels from '../components/homepage/buildModels'
+import TrustedBy from '../components/global/trusted-by';
 
 
 export default function Home({ data }) {
@@ -108,28 +109,7 @@ export default function Home({ data }) {
      
       <TryApi />
       <BuildModels />
-      <div>
-        <div className="px-5 py-10 md:pt-28">
-          <h2 className="text-black max-w-7xl block m-auto font-extrabold text-center">
-            TRUSTED BY
-          </h2>
-        </div>
-        <div className="max-w-4xl block m-auto">
-          <div className="grid grid-cols-1 m-auto block md:grid-cols-4 px-5 pb-0 md:pb-10">
-            <a href="https://coinshift.xyz/" target="_blank" rel="noreferrer"><img className="block mx-auto py-5" width={150} src="../assets/Coinshift.svg" alt="Coinshift logo" /></a>
-            <a href="https://www.yield.app/" target="_blank" rel="noreferrer"><img className="block mx-auto py-5" width={130} src="../assets/yieldapp-logo.svg" alt="Yield App logo" /></a>
-            <a href="https://picante.io/" target="_blank" rel="noreferrer"><img className="block mx-auto py-7" width={150} src="../assets/picante-logo.svg" alt="Picante logo" /></a>
-            <a href="https://exponent.cx/" target="_blank" rel="noreferrer"><img className="block mx-auto py-5" width={170} src="../assets/exponent.jpeg" alt="Exponent logo" /></a>
-          </div>
-          <div className="max-w-5xl block m-auto grid grid-cols-1 m-auto block md:grid-cols-5 px-5 pb-10 md:pb-20 md:px-0">
-            <a href="https://dlab.vc/" target="_blank" rel="noreferrer"><img className="py-5 block mx-auto" width={130} src="../assets/dlab-logo.svg" alt="DLAB logo" /></a>
-            <a href="https://www.longhash.com/en" target="_blank" rel="noreferrer"><img className="block mx-auto py-5 md:py-10" width={150} src="../assets/longhash-logo.svg" alt="Long Hash Ventures logo" /></a>
-            <a href="https://api3.org/" target="_blank" rel="noreferrer"><img className="block mx-auto py-5 md:py-10" width={130} src="../assets/API3-logo.svg" alt="API3 logo" /></a>
-            <a href="https://irulast.com/" target="_blank" rel="noreferrer"><img className="block mx-auto py-5 md:py-10" width={130} src="../assets/irulast-logo.svg" alt="Irulast logo" /></a>
-            <a href="https://docs.debtdao.finance/overview/what-is-debt-dao" target="_blank" rel="noreferrer"><img className="block mx-auto py-5 md:py-8" width={80} src="../assets/debt-dao-logo.png" alt="Debt dao logo" /></a>
-          </div>
-        </div>
-      </div>
+      <TrustedBy />
       <Subscribe />
       <Footer />
     </>
